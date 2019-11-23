9d92cef36ff5e90ece6b88aa9d36f79f4aa136e8
Brandon Rothel <rothel.2@wright.edu>
2019-11-23 18:56:00 -0500

    inital commit

---
+++ MotorizedCar.ino
@@ -0,0 +1,52 @@
+int RB = 5;
+int RF = 4;
+int LF = 0;
+int LB = 2;
+int LSpeed = 14;
+int RSpeed = 12;
+void setup() {
+  pinMode(LB, OUTPUT);
+  pinMode(LF, OUTPUT);
+  pinMode(RF, OUTPUT);
+  pinMode(RB, OUTPUT);
+  pinMode(LSpeed, OUTPUT);
+  pinMode(RSpeed, OUTPUT);
+  digitalWrite(LB, LOW);
+  digitalWrite(LF, LOW);
+  digitalWrite(RF, LOW);
+  digitalWrite(RB, LOW);
+  analogWrite(LSpeed, 1000);
+  analogWrite(RSpeed, 1000);
+  }
+
+void loop() {
+  delay(3000);
+  turnRight(0.5);
+  delay(10000);
+//  delay(3000);
+//  digitalWrite(LB, 1);
+//  delay(1000);
+//  digitalWrite(LB, 0);
+//  digitalWrite(LF, 1);
+//  delay(1000);
+//  digitalWrite(LF, 0);
+//  digitalWrite(RF, 1);
+//  delay(1000);
+//  digitalWrite(RF, 0);
+//  digitalWrite(RB, 1);
+//  delay(1000);
+//  digitalWrite(RB, 0);
+//  delay(10000);
+}
+
+void turnRight(float seconds){
+  digitalWrite(RB, 1);
+  digitalWrite(RF, 0);
+  digitalWrite(LB, 0);
+  digitalWrite(LF, 1);
+  delay(seconds*1000);
+  digitalWrite(RB, 0);
+  digitalWrite(RF, 0);
+  digitalWrite(LB, 0);
+  digitalWrite(LF, 0);
+}
